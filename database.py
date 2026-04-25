a0511ead638c9f0dd1ad9bd6511d80fe41b364c3
Akshay2602-Ak <takshaykumar26022004@gmail.com>
2026-04-25 14:27:26 +0530

    old codes

--- database.py
+++ database.py
@@ -1,63 +1,33 @@
 import os
 import psycopg2
-import sqlite3
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-
 def create_connection():
-    if DATABASE_URL:
-        # 🌐 Railway (Postgres)
-        return psycopg2.connect(DATABASE_URL)
-    else:
-        # 💻 Local (SQLite fallback)
-        return sqlite3.connect("local.db")
+    return psycopg2.connect(DATABASE_URL)
 
 
 def create_table():
     conn = create_connection()
     cursor = conn.cursor()
 
-    # ✅ Detect DB type
-    is_postgres = DATABASE_URL is not None
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS metrics (
+        id SERIAL PRIMARY KEY,
+        cpu REAL,
+        memory REAL,
+        disk REAL,
+        timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )
+    """)
 
-    if is_postgres:
-        cursor.execute("""
-        CREATE TABLE IF NOT EXISTS metrics (
-            id SERIAL PRIMARY KEY,
-            cpu REAL,
-            memory REAL,
-            disk REAL,
-            timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-        )
-        """)
-
-        cursor.execute("""
-        CREATE TABLE IF NOT EXISTS alerts (
-            id SERIAL PRIMARY KEY,
-            message TEXT,
-            timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-        )
-        """)
-    else:
-        # 🔥 SQLite version
-        cursor.execute("""
-        CREATE TABLE IF NOT EXISTS metrics (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            cpu REAL,
-            memory REAL,
-            disk REAL,
-            timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
-        )
-        """)
-
-        cursor.execute("""
-        CREATE TABLE IF NOT EXISTS alerts (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            message TEXT,
-            timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
-        )
-        """)
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS alerts (
+        id SERIAL PRIMARY KEY,
+        message TEXT,
+        timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )
+    """)
 
     conn.commit()
     conn.close()
